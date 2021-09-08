96003df3629d80730d0c43680064b0f5fc6d8a2e
d1-s <d.sub.second.01@gmail.com>
2021-09-08 21:51:26 +0900

    アソシエーションの記述

--- app/models/delivery_fee.rb
+++ app/models/delivery_fee.rb
@@ -5,4 +5,7 @@ class DeliveryFee < ActiveHash::Base
     { id: 3, name: '送料込み（出品者負担）' }
   ]
 
+  include ActiveHash::Associations
+  has_many :items
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :User
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :send_days
 end
