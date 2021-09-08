168f46ecdeff55c8b4665dc8de5e973d61575199
d1-s <d.sub.second.01@gmail.com>
2021-09-08 12:01:37 +0900

    エラーハンドリングの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  KANA_REGEX = /\A[ァ-ヴー]+\z/u
   validates :nickname, presence: true
+  validates :password, format: { with: PASSWORD_REGEX }
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true, format: { with: KANA_REGEX }
+  validates :first_name_kana, presence: true, format: { with: KANA_REGEX }
   validates :birthday, presence: true
 end
