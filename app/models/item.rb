319839a94ec328a98e5c3f2fa383ca60565b68a4
d1-s <d.sub.second.01@gmail.com>
2021-09-09 16:36:53 +0900

    エラーメッセージの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   belongs_to :send_days
 
   validates :item_name, :item_info, :image, presence: true
-  validates :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :send_days_id, numericality: { other_than: 1 }
+  validates :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :send_days_id, numericality: { other_than: 1, message: "can't be blank" }
   with_options presence: true, format: { with: /\A[0-9]+\z/ } do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
                       presence: { message: "can't be blank" }
