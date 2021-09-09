2a212e595babb1e2f080fb1643a795774ade26b2
d1-s <d.sub.second.01@gmail.com>
2021-09-09 12:58:48 +0900

    商品情報をデータベースへ保存する、ビューとコントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_info, :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :send_days_id, :price).merge(user_id: current_user.id)
   end
 end
