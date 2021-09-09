786a95badd388ebf03f46be5ed167b717cd09ab1
d1-s <d.sub.second.01@gmail.com>
2021-09-09 12:13:44 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,11 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :send_days
+
+  validates :item_name, :item_info, :image, presence: true
+  validates :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :send_days_id, numericality: { other_than: 1 }
+  with_options presence:true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
+                      presence: { message: "can't be blank" }
+  end
 end
