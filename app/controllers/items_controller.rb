72e632cb32ec9390ac628fc844e25f14c5efa96f
d1-s <d.sub.second.01@gmail.com>
2021-09-09 14:19:29 +0900

    出品ボタンで商品情報をDBに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    Item.create!(item_params)
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :User
+  belongs_to :user
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
 
   validates :item_name, :item_info, :image, presence: true
   validates :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :send_days_id, numericality: { other_than: 1 }
-  with_options presence:true, format: { with: /\A[0-9]+\z/ } do
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
                       presence: { message: "can't be blank" }
   end
