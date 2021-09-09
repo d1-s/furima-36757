69339e92cf50184c319347377b13354d5a8a4d5e
d1-s <d.sub.second.01@gmail.com>
2021-09-09 21:15:34 +0900

    Rubocopの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -64,7 +64,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceが「10000000」以上では出品できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
