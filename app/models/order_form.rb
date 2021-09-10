13416491a379352649acd5415139c4feaafa911f
d1-s <d.sub.second.01@gmail.com>
2021-09-10 22:36:47 +0900

    Formオブジェクト「order_form.rb」作成

---
+++ app/models/order_form.rb
@@ -0,0 +1,19 @@
+class OrderForm
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building, :tel_number, :user_id, :item_id, :order_id
+
+  with_options presence: true do
+    validates :postal_code
+    validates :municipalities
+    validates :address
+    validates :tel_number
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :building
+
+  def save
+    Order.create(user_id: user.id, item_id: item.id)
+    DeliveryInfo.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building: building, tel_number: tel_number, order_id: order.id)
+  end
+
+end
