c74d1d8a63887ccbd134835913f3abeeea110a0d
d1-s <d.sub.second.01@gmail.com>
2021-09-10 13:47:38 +0900

    ログインの有無、出品者かどうかで遷移先が変わる設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.includes(:user).order(created_at: 'DESC')
@@ -23,6 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
