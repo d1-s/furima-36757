1553a17067f7d6e19c45ebbbef41b08c8c23b9a9
d1-s <d.sub.second.01@gmail.com>
2021-09-11 15:31:48 +0900

    createアクションの処理を記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,5 +6,18 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = OrderForm.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :municipalities, :address, :building, :tel_number)
+  end
+
 end
