90d7e3615087b1cb77f042869c1f71422b2adc0d
d1-s <d.sub.second.01@gmail.com>
2021-09-11 20:40:45 +0900

    ログイン状態でも自身の出品した商品の購入画面にはいけず、トップページに遷移する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,7 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new
+    redirect_to root_path if current_user.id == @item.user_id
   end
 
   def create
