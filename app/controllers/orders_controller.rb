aa99a50c429b9f66f45881e1b09b91ba6d0a5fec
d1-s <d.sub.second.01@gmail.com>
2021-09-11 20:07:13 +0900

    バリデーションに引っ掛かるとエラーメッセージが表示される

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,12 +2,15 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @order = OrderForm.new
+    @order_form = OrderForm.new
   end
 
   def create
-    @order = OrderForm.new(order_params)
-    if @order.save
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new(order_params)
+    # binding.pry
+    if @order_form.valid?
+      @order_form.save
       redirect_to root_path
     else
       render :index
